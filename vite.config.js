e73d2248d32fd3d12efbc24dae2948c4103ef5e2
Russell Soza <endosoza1@gmail.com>
2025-04-21 22:06:29 -0600

    Fix: Cambio para el deploy de la aplicacion

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
-  server: {
-    port: 3000,
-  },
-})
+// https://vite.dev/config/
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      outDir: 'build'
+    },
+    server: {
+      port: 3000, 
+    },
+  });
+}
