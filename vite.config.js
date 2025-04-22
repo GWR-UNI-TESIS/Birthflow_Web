a61fdf07a6d85e0d0f4023b367dbd93d1f85c9bc
Russell Soza <endosoza1@gmail.com>
2025-04-21 21:54:31 -0600

    Feat: Cambiando el vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [react()],
-    build: {
-      outDir: 'build'
-    },
-    server: {
-      port: 3000, 
-    },
-  });
-}
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
+  server: {
+    port: 3000,
+  },
+})
